7cf897c39284eb7ad93677f1427df3b05f121497
Ruben Marin Grez <rumaringr@gmail.com>
2025-07-02 09:43:09 +0200

    QLearning Get current state and choose action implemented

--- LaserTagBox/Model/Mind/QLearningPlayerMind.cs
+++ LaserTagBox/Model/Mind/QLearningPlayerMind.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using LaserTagBox.Model.Shared;
@@ -5,6 +6,7 @@ using Mars.Interfaces.Environments;
 using Mars.Numerics;
 using System.IO;
 using System.Text.Json;
+
 
 namespace LaserTagBox.Model.Mind;
 // author Dominik Wiesendanger, Ruben Marin Grez
@@ -20,33 +22,39 @@ public class QLearningPlayerMind : AbstractPlayerMind
 
     private static double explorationRate = 0.2;
     
-    
+    private readonly Random _random = new();
     
     //Q-Table to store state-action values
-    //tragt Flagge, Gegner in der Nähe, teammate in der Nähe, eigene Flagge in der Nähe, ExplosiveBarrel in der Nähe, Stance
-    private double[][][][][][] QTable = new double[2][][][][][]; 
+    //tragt Flagge, Gegner in der Nähe, teammate in der Nähe, eigene Flagge in der Nähe, ExplosiveBarrel in der Nähe, Stance, actionPoints
+    private double[][][][][][][][] QTable = new double[1][][][][][][][]; 
     
     public override void Init(PlayerMindLayer mindLayer)
     {
         _mindLayer = mindLayer;
         
-        if (!LoadQTable("../../../Model/QTable.json"))
+        // Now QTable is double[1][][][][][][][] (8 dimensions)
+        for (int i = 0; i < QTable.Length; i++)
         {
-            for(int m = 0; m < 2; m++)
+            QTable[i] = new double[1][][][][][][];
+            for (int j = 0; j < QTable[i].Length; j++)
             {
-                QTable[m] = new double[2][][][][];
-                for(int f = 0; f < 2; f++)
+                QTable[i][j] = new double[1][][][][][];
+                for (int k = 0; k < QTable[i][j].Length; k++)
                 {
-                    QTable[m][f] = new double[2][][][];
-                    for(int e = 0; e < 2; e++)
+                    QTable[i][j][k] = new double[1][][][][];
+                    for (int l = 0; l < QTable[i][j][k].Length; l++)
                     {
-                        QTable[m][f][e] = new double[2][][];
-                        for(int t = 0; t < 2; t++)
+                        QTable[i][j][k][l] = new double[1][][][];
+                        for (int m = 0; m < QTable[i][j][k][l].Length; m++)
                         {
-                            QTable[m][f][e][t] = new double[3][]; // Stance: Standing, Kneeling, Lying
-                            for(int s = 0; s < 3; s++)
+                            QTable[i][j][k][l][m] = new double[1][][];
+                            for (int n = 0; n < QTable[i][j][k][l][m].Length; n++)
                             {
-                                QTable[m][f][e][t][s] = new double[15]; 
+                                QTable[i][j][k][l][m][n] = new double[1][];
+                                for (int o = 0; o < QTable[i][j][k][l][m][n].Length; o++)
+                                {
+                                    QTable[i][j][k][l][m][n][o] = new double[19];
+                                }
                             }
                         }
                     }
@@ -61,15 +69,37 @@ public class QLearningPlayerMind : AbstractPlayerMind
         
         //TODO implement Q-Learning logic
         //1. Get current state
+        var hasFlag = Body.CarryingFlag; 
+        var enemies = Body.ExploreEnemies1();
+        var teammates = Body.ExploreTeam();
+        var ownFlagInSight = Body.ExploreFlags2().FirstOrDefault(f => f.Team == Body.Color && f.PickedUp == false).PickedUp;
+        var explosiveBarrels = Body.ExploreExplosiveBarrels1();
+        var stance = Body.Stance;
+        var actionPoints = Body.ActionPoints;
         //2. Choose action (exploration vs exploitation)
+        var actions = QTable[hasFlag ? 1 : 0][enemies.Count > 0 ? 1 : 0][teammates.Count > 0 ? 1 : 0][ownFlagInSight ? 1 : 0][explosiveBarrels.Count > 0 ? 1 : 0][(int)stance][actionPoints].ToList();        //3. calculate reward
+        
+        int action = getAction(actions);
+        
+        if (_random.NextDouble() < explorationRate)
+        {
+            action = _random.Next(0, actions.Count); 
+        }
+        
         //3. calculate reward
+        
+        int reward = calculateReward();
+        
         //4. Update Q-Table
         //5. save Q-Table
     }
     
+    private int getAction(List<double> actions)
+    {
+        return actions.IndexOf(actions.Max());
+    }
+    
     //Q-Learning methods
-    
-    
     private int calculateReward()
     {
         int reward = 0;
@@ -87,7 +117,7 @@ public class QLearningPlayerMind : AbstractPlayerMind
     {
         if (File.Exists(filePath))
         {
-            QTable = JsonSerializer.Deserialize<double[][][][][][]>(File.ReadAllText(filePath));
+            QTable = JsonSerializer.Deserialize<double[][][][][][][][]>(File.ReadAllText(filePath));
             return true;
         }
         else
